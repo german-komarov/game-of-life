cb6f2510595e650cbb340c70180de2c91392ce73
german.komarov <german.komarov@ufaz.az>
2022-06-06 19:53:14 +0400

    Khumar : isAlive function

--- game.cpp
+++ game.cpp
@@ -67,6 +67,49 @@ public:
 
 };
 
+const int GAME_WIDTH = 640;
+const int GAME_HEIGHT = 480;
+
+bool isAlive(
+        std::array<std::array<int, GAME_HEIGHT>, GAME_WIDTH>& game,
+        const int x,
+        const int y)
+{
+
+    int alive = 0;
+
+    // testing the left
+    if(x > 0 && game[x-1][y] == 1) alive+=1;
+    // testing the right
+    if(x < GAME_WIDTH-1 && game[x+1][y] == 1) alive+=1;
+    // testing top
+    if(y > 0 && game[x][y-1] == 1) alive+=1;
+    // testing bottom
+    if(y < GAME_HEIGHT-1 && game[x][y+1] == 1) alive+=1;
+
+
+    // testing top left
+    if(y > 0 && x > 0 && game[x-1][y-1] == 1) alive+=1;
+    // testing top right
+    if(y > 0 && x < GAME_WIDTH-1 && game[x+1][y-1] == 1) alive+=1;
+    // testing bottom left
+    if(y < GAME_HEIGHT-1 && x > 0 && game[x-1][y+1] == 1) alive +=1;
+    // testing bottom right
+    if(y < GAME_HEIGHT-1 && x < GAME_WIDTH-1 && game[x+1][y+1] == 1) alive+=1;
+
+    // alive and fewer than 2 die
+    if(game[x][y] == 1 && alive < 2) return false;
+    // alive and 2 or 3 then live
+    if(game[x][y] == 1 && (alive == 2 || alive == 3)) return true;
+    // more than 3 live die
+    if(alive > 3) return false;
+    // 3 alive and point is dead, come to life
+    if(game[x][y] == 0 && alive == 3) return true;
+
+    return false;
+
+}
+
 int main() {
 
 }
