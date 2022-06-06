cda49183f0a634e1b61c224d5c1cbeadae4707d7
german.komarov <german.komarov@ufaz.az>
2022-06-06 19:07:57 +0400

    German : Refactoring and removing unnecessary functionality;

--- src/gameOfLife.c
+++ src/gameOfLife.c
@@ -1,16 +1,7 @@
 #include "../include/gameOfLife.h"
 
 int main() {
-    char option[64];
-
-    printf(" 1. Random board\n");
-
-    while (atoi(option) != 1 && atoi(option) != 2) {
-        fgets(option, sizeof(option), stdin);
-    }
-
     initializeBoard();
-
 
     while (1) {
         clrscr();
