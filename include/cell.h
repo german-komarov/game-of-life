7e92ca753e4aa4cc871d0cb9bbdcd95c60363667
german.komarov <german.komarov@ufaz.az>
2022-06-06 19:48:12 +0400

    German : Switching project to CPP plus OOP (Board class);

--- include/cell.h
+++
@@ -1,5 +0,0 @@
-#include "boardSize.h"
-
-int aliveNeighbors(int board[MAXROWS][MAXCOLUMNS], int row, int column);
-
-int aliveNeighborsInRow(int board[MAXROWS][MAXCOLUMNS], int testRow, int row, int column);
